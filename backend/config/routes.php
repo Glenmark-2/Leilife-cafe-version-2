a00cb8813624d148e1b8ba9a675aaf95909c3637
Ennovie Evangelista <ennovie23@gmail.com>
2025-12-07 20:25:20 +0800

    added feature/change_password

--- backend/config/routes.php
+++ backend/config/routes.php
@@ -5,4 +5,5 @@ return [
     '404' => __DIR__ . '/../../pages/404.php',
     'sign_up' => __DIR__. '/../../pages/sign_up.php',
     'menu' => __DIR__. '/../../pages/menu.php',
+    'change_password' => __DIR__. '/../../pages/change_password.php',
 ];
