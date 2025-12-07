e2024de08982ec33b90474931c2892995738adad
Ennovie Evangelista <ennovie23@gmail.com>
2025-12-07 23:15:11 +0800

    add feature/profile

--- backend/config/routes.php
+++ backend/config/routes.php
@@ -6,4 +6,5 @@ return [
     'sign_up' => __DIR__. '/../../pages/sign_up.php',
     'menu' => __DIR__. '/../../pages/menu.php',
     'change_password' => __DIR__. '/../../pages/change_password.php',
+    'profile' => __DIR__. '/../../pages/profile.php',
 ];
---
+++ partials/order_history_card.php
@@ -0,0 +1,15 @@
+<div class="info" style="width: 100%; padding: 15px; background: #fdfbf6;">
+    <p class="label">Order ID: <span style="font-weight: normal;">#1001</span></p>
+    <p class="label">Date: <span style="font-weight: normal;">Dec 5, 2025</span></p>
+    <p class="label">Items:</p>
+    <ul style="margin: 0 0 5px 15px; padding: 0; font-weight: normal;">
+        <li>Cheesy Bacon & Egg</li>
+        <li>Espresso</li>
+    </ul>
+    <div id="price-div">
+        <p>Total: ₱300.00</p>
+        <p style="color: green; font-weight: bold;">Delivered</p>
+    </div>
+</div>
+
+<!-- wla pa ung reorder, track, write a review buttons -->
