a9a738516c888721c0774e603e060c255dc08703
glenmark <coddybanks178@gmail.com>
2025-12-08 02:05:34 +0800

    created order tracking page

---
+++ pages/order_tracking.php
@@ -0,0 +1,77 @@
+<div class="container text-center mt-3">
+
+    <p class="order-number">Your Order #ORD-20251114-A31E2C</p>
+
+    <!-- PROGRESS STEPS -->
+    <div class="progress-steps d-flex justify-content-between mt-4">
+        <div class="step">
+            <div class="prog-num active">1</div>
+            <p>Queuing</p>
+        </div>
+
+        <div class="step">
+            <div class="prog-num">2</div>
+            <p>Preparing</p>
+        </div>
+
+        <div class="step">
+            <div class="prog-num">3</div>
+            <p>Out for delivery</p>
+        </div>
+
+        <div class="step">
+            <div class="prog-num">4</div>
+            <p>Delivered</p>
+        </div>
+    </div>
+
+    <!-- ETA + DELIVERY DETAILS -->
+    <div class="row justify-content-center align-items-start mt-5 g-4">
+
+        <!-- LEFT SIDE -->
+        <div class="col-12 col-md-6 text-center mb-4 mb-md-0">
+            <p class="eta-title">Estimated time of delivery</p>
+            <p class="eta">10 - 15 mins</p>
+
+            <div class="product-img-wrapper">
+                <img src="/Leilife_2nd/public/assets/motorbike.png" alt="Motorbike" class="product-img">
+            </div>
+        </div>
+
+        <!-- RIGHT SIDE -->
+        <div class="col-12 col-md-6 delivery-col">
+
+            <p class="section-title text-center">Delivery details</p>
+
+            <div class="dev-details">
+                <div class="detail-item">
+                    <img src="/Leilife_2nd/public/assets/pin.png" class="icon">
+                    <span>10880 Malibu Point, Malibu, CA 90265</span>
+                </div>
+
+                <div class="detail-item mt-3">
+                    <img src="/Leilife_2nd/public/assets/credit-card.png" class="icon">
+                    <span>Gcash</span>
+                </div>
+            </div>
+
+            <p class="section-title mt-4 text-center">Order details</p>
+
+            <div class="ord-dtls">
+                <div class="detail-item">
+                    <p>1 × Sweet & Sour Pork — ₱135.00</p>
+                </div>
+
+                <div class="detail-item mt-3">
+                    <p>Total: ₱165.00</p>
+                </div>
+            </div>
+
+            <div class="text-center mt-4">
+                <button class="btn btn-primary-custom">Cancel</button>
+            </div>
+
+        </div>
+
+    </div>
+</div>
